2b67dd889b9ceaba7b49654112e9cb743fb492ba
Beth Carretta <bethcarretta@gmail.com>
2023-05-19 15:30:26 -0400

    login/logout user route logic

--- app.py
+++ app.py
@@ -24,6 +24,30 @@ def index():
     """Redirects to registration form"""
     return redirect('/register')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    """Form to handle user login"""
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome back, {user.first_name}!", 'success')
+            session['user_id'] = user.id
+            return redirect('/secret')
+        else:
+            form.username.errors = ['Please enter a valid username and password']
+    return render_template('login_form.html', form=form)        
+
+@app.route('/logout', methods=['POST'])
+def logout_user():
+    """Logs out user and removes user from session"""
+    session.pop('user_id')
+    flash('Goodbye!', 'info')
+    return redirect('/login')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
     """Form to handle new user registration"""
